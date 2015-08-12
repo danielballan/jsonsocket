6621b598007990a8ba001526cdeb120e3392613c
danielballan <daniel.b.allan@gmail.com>
2015-08-12 17:26:44 -0400

    FIX: Python 3 compatibility

--- jsonsocket.py
+++ jsonsocket.py
@@ -106,20 +106,20 @@ class Client(object):
 def _send(socket, data):
   try:
     serialized = json.dumps(data)
-  except (TypeError, ValueError), e:
+  except (TypeError, ValueError) as e:
     raise Exception('You can only send JSON-serializable data')
   # send the length of the serialized data first
-  socket.send('%d\n' % len(serialized))
+  socket.send('{0}\n'.format(len(serialized)).encode('utf8'))
   # send the serialized data
-  socket.sendall(serialized)
+  socket.sendall(serialized.encode('utf8'))
 
 def _recv(socket):
   # read the length of the data, letter by letter until we reach EOL
   length_str = ''
-  char = socket.recv(1)
+  char = socket.recv(1).decode()
   while char != '\n':
     length_str += char
-    char = socket.recv(1)
+    char = socket.recv(1).decode()
   total = int(length_str)
   # use a memoryview to receive the data chunk by chunk efficiently
   view = memoryview(bytearray(total))
@@ -128,7 +128,7 @@ def _recv(socket):
     recv_size = socket.recv_into(view[next_offset:], total - next_offset)
     next_offset += recv_size
   try:
-    deserialized = json.loads(view.tobytes())
-  except (TypeError, ValueError), e:
+    deserialized = json.loads(view.tobytes().decode())
+  except (TypeError, ValueError) as e:
     raise Exception('Data received was not in JSON format')
   return deserialized
